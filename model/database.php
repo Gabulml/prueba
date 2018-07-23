317509714116d172e661972a7117b125de277d1d
Gabulml <gabyslp_lml@hotmail.com>
2018-07-22 22:11:05 -0500

    Update database.php
    nueva app

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=pruebamysql.mysql.database.azure.com;dbname=alumnos;charset=utf8', 'myadmingaby@pruebamysql', 'Cat221995');
+        $pdo = new PDO('mysql:host=pruebamysql.mysql.database.azure.com;dbname=alumnos;charset=utf8', 'GabyCuaspud@pruebamysql', 'Cat221995');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
